93e3d55f1a26dea480a036e544b5be6be1c45338
EDUARDO FELIPE SANTOS PINHEIRO <eduardo.pinheiro4@fatec.sp.gov.br>
2021-07-21 20:57:01 -0300

    Start searchOptions

--- javascript/searchMenu/methods.js
+++ javascript/searchMenu/methods.js
@@ -30,3 +30,74 @@ const scrollToSection = (key) => {
     }
   }
 }
+
+const addSearchOptionInSectionHTML = (elementToInsertOption, option) => {
+  const {
+    title,
+    scrollToKey,
+  } = option;
+
+  if (
+    elementToInsertOption &&
+    typeof title === 'string' &&
+    typeof scrollToKey === 'string'
+  ) {
+    elementToInsertOption.innerHTML += `<li data-scroll-to-key="${scrollToKey}" class="option button-base">${title}</li>`;
+  }
+}
+
+const resetInnerHTMLSearchSections = () => {
+  optionsSectionOne.innerHTML = '';
+  optionsSectionTwo.innerHTML = '';
+  optionsSectionThree.innerHTML = '';
+  optionsSectionFour.innerHTML = '';
+}
+
+const renderSearchOptions = (searchText) => {
+  fetch('../../data/menuOptions.json')
+    .then(response => response.json())
+    .then(menuOptions => {
+      resetInnerHTMLSearchSections();
+      let searchedMenuOptions = [];
+
+      if (typeof searchText === 'string') {
+        menuOptions.forEach(menuOption => {
+          const searchTextLowerCase = searchText.toLowerCase();
+          const optionTitleLowerCase = menuOption.title.toLowerCase();
+          if (optionTitleLowerCase.includes(searchTextLowerCase)) {
+            searchedMenuOptions.push(menuOption);
+          }
+        });
+      } else {
+        searchedMenuOptions.push(...menuOptions);
+      }
+
+      let menuOptionsSectioned = [];
+      const sliceIterationsQuantity = Math.ceil(searchedMenuOptions?.length / 5);
+      
+      for (let index = 0; index < sliceIterationsQuantity; index++) {
+        const optionsSection = searchedMenuOptions.splice(0, 5);
+        menuOptionsSectioned.push(optionsSection);
+      }
+
+      menuOptionsSectioned.map((optionsSection, sectionIndex) => {
+        optionsSection.map(option => {
+          switch (sectionIndex) {
+            case 0:
+              addSearchOptionInSectionHTML(optionsSectionOne, option);
+              break;
+            case 1:
+              addSearchOptionInSectionHTML(optionsSectionTwo, option);
+              break;
+            case 2:
+              addSearchOptionInSectionHTML(optionsSectionThree, option);
+              break;
+            case 3:
+              addSearchOptionInSectionHTML(optionsSectionFour, option);
+              break;
+          }
+        });
+      });
+    });
+}
+renderSearchOptions('grid');
